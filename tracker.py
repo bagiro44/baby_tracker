ff04d08ba6653ece3c543431991f9b817bafc81c
bagiro44 <bagiro44@bk.ru>
2025-11-23 01:15:58 +0300

    upd

--- tracker.py
+++
@@ -1,9 +0,0 @@
-"""
-Инициализация трекера для избежания циклических импортов
-"""
-
-from leo_bot.database.sheets import GoogleSheetsBabyTracker
-from leo_bot.config import GOOGLE_CREDENTIALS_FILE, SPREADSHEET_NAME
-
-# Создаем экземпляр трекера
-tracker = GoogleSheetsBabyTracker(GOOGLE_CREDENTIALS_FILE, SPREADSHEET_NAME)
